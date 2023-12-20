f6f6f9edf4c2997a65a6af74c1735dc1304a0769
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-20 19:55:27 +0000

    change

--- 1/1.js
+++ 1/1.js
@@ -1,1 +1,24 @@
-console.log("Ayushman")
+// Variables
+const accountId = 144553  // not change after declear
+// accountId = 2 // not allowed
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"  // This also declear variable but Dont use it
+let accountState;
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// Alowed
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+// For print accountId value in console
+console.log(accountId);
+
+
+// This is use for printing multiple variables in form of table
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
