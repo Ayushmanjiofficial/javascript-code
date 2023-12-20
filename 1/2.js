4e208e4828b93df21e7964a02b77ef38295177a2
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-20 20:16:34 +0000

    change

---
+++ 1/2.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high but it run
+
+console.log("Ayushman")
+
+
+let name = "Ayushman"  // String
+let age = 18  // Number
+let isLoggedIn = false  //  Boolean
+let state;  // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object is other data type
+
+
+console.log(typeof "Ayushman"); // String
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
