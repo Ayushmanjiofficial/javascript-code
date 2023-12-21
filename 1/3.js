4f3c748df6ade816fc793a44de8321fc63344f53
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-21 08:53:08 +0000

    conversion

--- 1/3.js
+++ 1/3.js
@@ -1,22 +1,25 @@
-let score = "hitesh"
+let score = "Ayushman"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
-
+// convert to number :-
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
+//  null => 0
+//  undefined => NaN
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "Ayushman"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
+// convert to boolean
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
