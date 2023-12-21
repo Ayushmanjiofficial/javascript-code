8e0031deb67b9a55a94618d451b272792ad12c56
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-21 13:28:19 +0000

    data type

--- 1/3.js
+++ 1/3.js
@@ -1,11 +1,11 @@
 let score = "Ayushman"
 
-console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof score);  //-> String 
+console.log(typeof(score)); //-> String
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+//console.log(typeof valueInNumber); //-> Number
+//console.log(valueInNumber); //-> NaN
 
 // convert to number :-
 // "33" => 33
@@ -17,7 +17,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Ayushman"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn); //-> true
 
 // convert to boolean
 // 1 => true; 0 => false
@@ -27,37 +27,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(stringNumber); //-> 33
+// console.log(typeof stringNumber); //-> String
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); //-> -3
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// console.log(2+2); //-> 4
+// console.log(2-2); //-> 0
+// console.log(2*2); //-> 4
+// console.log(2**3); //-> 8
+// console.log(2/3); //-> 0.66
+// console.log(2%3); //-> 2
 
-let str1 = "hello"
-let str2 = " hitesh"
+let str1 = "Hello"
+let str2 = " Ayushman"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); //-> Hello Ayushman
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); //-> 12
+// console.log(1 + "2"); //-> 12
+// console.log("1" + 2 + 2); //-> 122
+// console.log(1 + 2 + "2"); //-> 32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(true); //-> true
+// console.log(+true); //-> 1
+// console.log(+""); //-> 0
 
 let num1, num2, num3
 
@@ -65,7 +66,7 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+console.log(gameCounter); //-> 101
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
