f4a43f5cf25f12010c7c8e09c32b0d7cbffed1e0
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-21 13:44:24 +0000

    check

---
+++ 1/4.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1); //-> true
+// console.log(2 >= 1); //-> true
+// console.log(2 < 1); //-> false
+// console.log(2 == 1); //-> false
+// console.log(2 != 1); //-> true
+
+
+// console.log("2" > 1); //it use ascii value
+// console.log("02" > 1);
+
+console.log(null > 0); //-> false, null convert to 0
+console.log(null == 0); //-> false
+console.log(null >= 0); //-> true, null convert to 0
+
+console.log(undefined == 0); //-> false
+console.log(undefined > 0); //-> false
+console.log(undefined < 0); //-> false
+
+// == it only check number
+// === it also check data type 
+
+console.log("2" == 2); //-> true
+console.log("2" === 2); //-> false
