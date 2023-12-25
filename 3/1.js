6bed15ca3dc7b939f10e97a72a17462a99359595
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-25 18:48:24 +0000

    func

--- 3/1.js
+++ 3/1.js
@@ -7,7 +7,7 @@ function sayMyName(){
     console.log("H");
 }
 
-// sayMyName()
+// sayMyName() // calling the function
 
 // function addTwoNumbers(number1, number2){
 
@@ -23,7 +23,7 @@ function addTwoNumbers(number1, number2){
 
 const result = addTwoNumbers(3, 5)
 
-// console.log("Result: ", result);
+console.log("Result: ", result); //-> Result:  8
 
 
 function loginUserMessage(username = "sam"){
@@ -34,30 +34,32 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh")) //-> hitesh just logged in
+// console.log(loginUserMessage()) //-> undefined just logged in
+// console.log(loginUserMessage()) //-> sam just logged in // by using defalt value
 
 
 function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+// console.log(calculateCartPrice(200, 400, 500, 2000)) // val1=200, val2=400, rest in 'num1'=[500, 2000].
 
+// defining obj. in fuction:- 
 const user = {
     username: "hitesh",
     prices: 199
 }
 
 function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
 }
 
-// handleObject(user)
+handleObject(user) //-> Username is hitesh and price is 199
 handleObject({
     username: "sam",
-    price: 399
-})
+    prices: 399
+}) //-> Username is sam and price is 399 // giving derect obj.
 
 const myNewArray = [200, 400, 100, 600]
 
@@ -65,5 +67,5 @@ function returnSecondValue(getArray){
     return getArray[1]
 }
 
-// console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+// console.log(returnSecondValue(myNewArray)); //-> 400
+console.log(returnSecondValue([200, 300, 500, 1000])); //-> 300
