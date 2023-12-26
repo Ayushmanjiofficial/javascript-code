23160b186850aa4d4c3437cd345a89323cc3fa51
Ayushman Sharma <154364631+Ayushmanjiofficial@users.noreply.github.com>
2023-12-26 21:18:19 +0000

    loops

---
+++ 5/7.js
@@ -0,0 +1,12 @@
+// itrate by map: it not check any condition.
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+// Channing:-
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
